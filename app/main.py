00929324ec7338fdf3c8207f15994bc4ca41910e
Mouli <baranivel.l2023@vitstudent.ac.in>
2026-09-21 17:39:37 +0530

    Add API health checkup

--- app/main.py
+++ app/main.py
@@ -1,5 +1,20 @@
-def main():
-    print("Production AI project started")
+from fastapi import FastAPI
 
-if __name__ == "__main__":
-    main()
+app = FastAPI(
+    title = "Product AI API",
+    description = "Production oriented AI inference service",
+    version = "0.1.0"
+)
+
+@app.get("/")
+def root():
+    return{
+        "message" : "Product AI API is running",
+        "version" : "0.1.0"
+    }
+
+@app.get("/health")
+def health():
+    return {
+        "status" : "Healthy"
+    }
